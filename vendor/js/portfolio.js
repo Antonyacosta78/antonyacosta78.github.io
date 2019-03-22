d7bafc69c745d76532ed87aa34d5afc47b29f1de
Antony Acosta <antonyacosta78@gmail.com>
2019-03-22 01:43:47 -0300

    Started the main body!

--- vendor/js/portfolio.js
+++ vendor/js/portfolio.js
@@ -3,5 +3,17 @@ $(document).ready(() => {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
+
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+      
+        $('html, body').animate(
+          {
+            scrollTop: $($(this).attr('href')).offset().top,
+          },
+          500,
+          'linear'
+        )
+      })
     
 });
