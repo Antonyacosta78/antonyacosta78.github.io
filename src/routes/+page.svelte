60dd7ed4d4e91ce2abe137a0fb9647eedd8c3650
Antony Acosta <antonyacosta78@gmail.com>
2025-06-20 01:17:59 -0300

    Fixed spelling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,8 @@
 <div class="flex justify-center h-screen w-screen bg-primary">
   <div class="place-self-center w-160 text-center text-secondary">
     <h2 class="text-5xl font-bold">Uh Oh!</h2>
-    
    <p class="text-xl">My portfolio isn't ready just yet! do come back in a week or so to check on it!</p>
-   <p class="text-muted text-md">Thank you for your interest though, if you have any questions or want to set an interview, please do so!</p>
+   <p class="text-muted text-md">Thank you for your interest though! If you have any questions or want to set an interview, please do so!</p>
    
    <br>
    <ul>
