d3bfa63b865ba38d77e1356c257cc6bb100fe317
Antony Acosta <antonyacosta78@gmail.com>
2025-06-19 22:33:27 -0300

    First Commit, project set up, created 'not ready' page!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+
+<div class="flex justify-center h-screen w-screen bg-primary">
+  <div class="place-self-center w-160 text-center text-secondary">
+    <h2 class="text-5xl font-bold">Uh Oh!</h2>
+    
+   <p class="text-xl">My portfolio isn't ready just yet! do come back in a week or so to check on it!</p>
+   <p class="text-muted text-md">Thank you for your interest though, if you have any questions or want to set an interview, please do so!</p>
+   
+   <br>
+   <ul>
+      <li>
+        <a class="text-accent-secondary hover:text-accent" href="mailto:antonyacosta78@gmail.com" aria-label="Email me link">Mail me!</a>
+      </li>
+      <li>
+        <a class="text-accent-secondary hover:text-accent" href="https://linkedin.com/in/antony-acosta" target="_blank">Talk to me on LinkedIn!</a>
+      </li>
+    </ul>
+  </div>
+</div>
