98c0cf1191d4ea848f25ae8c29b12ee6e3d29d89
Antony Acosta <antonyacosta78@gmail.com>
2025-06-20 01:18:17 -0300

    added adapter options

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html',
+			pages: 'build/'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
