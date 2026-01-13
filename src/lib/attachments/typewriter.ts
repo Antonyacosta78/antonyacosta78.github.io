58ba760a5359cb14b4d10801da807ae7e5def813
Antony Acosta <antonyacosta78@gmail.com>
2026-01-13 12:15:58 -0300

    delayer util + delayer for typewriter

--- src/lib/attachments/typewriter.ts
+++ src/lib/attachments/typewriter.ts
@@ -1,21 +1,25 @@
-import type { Attachment } from "svelte/attachments";
-import TypeIt, { type Options as TypeItOptions } from "typeit";
+import type { DelayerTime } from '$lib/utils/delayer';
+import delayer from '$lib/utils/delayer';
+import type { Attachment } from 'svelte/attachments';
+import TypeIt, { type Options as TypeItOptions } from 'typeit';
 
-export default function typewriter(options: TypeItOptions = {}): Attachment<HTMLElement> {
-  
-  return (element) => {
-    // we get the content of the HTML then we delete it so it can be rewritten by TypeIt
-    const text = element.innerHTML;
-    element.innerHTML = "";
-    
-    const afterComplete = (instance: { destroy: () => void; }) => {
-      instance.destroy();
-      if(options.afterComplete) {
-        options.afterComplete();
-      }
-    }
-    
-    new TypeIt(element, {
+export default function typewriter(
+	options: TypeItOptions = {},
+	waitUntil?: DelayerTime
+): Attachment<HTMLElement> {
+	return (element) => {
+		// we get the content of the HTML then we delete it so it can be rewritten by TypeIt
+		const text = element.innerHTML;
+		element.innerHTML = '';
+
+		const afterComplete = (instance: { destroy: () => void }) => {
+			instance.destroy();
+			if (options.afterComplete) {
+				options.afterComplete();
+			}
+		};
+
+		const writer = new TypeIt(element, {
 			strings: text,
 			waitUntilVisible: true,
 			lifeLike: false,
@@ -24,6 +28,14 @@ export default function typewriter(options: TypeItOptions = {}): Attachment<HTML
 			cursorChar: '_',
 			...options,
 			afterComplete
-		}).go();
-  }
+		});
+
+		if (!waitUntil) {
+			writer.go();
+		} else {
+			delayer(waitUntil).then(() => {
+				writer.go();
+			});
+		}
+	};
 }
